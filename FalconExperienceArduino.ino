81de29b42dedc461df0f5514b678b8762b40bdfe
maximelucquin <maxime.lucquin@gmail.com>
2018-10-08 22:27:53 +0200

    Decode du string en entré partiellement terminé

--- FalconExperienceArduino.ino
+++ FalconExperienceArduino.ino
@@ -5,9 +5,27 @@
   This example code is in the public domain.
 */
 
+String inputName[] = {"B1","B2","B3","B4","B5","B6"};
+int inputPin[] = {1,2,3,4,5,6};
+int outputSize = 6;
+String outputName[] = {"L1","L2","L3","L4","L5","L6"};
+int outputPin[] = {7,8,9,10,11,13};
+
 String serialResponse = "";
 void setup() {
   Serial.begin(9600);
+  Serial.println("Init...");
+  
+  /*for (int thisPin = 0; thisPin < sizeof(inputPin); thisPin++) {
+    pinMode(inputPin[thisPin], INPUT);
+  }*/
+
+  for (int thisPin = 0; thisPin < outputSize; thisPin++) {
+    pinMode(outputPin[thisPin], OUTPUT);
+  }
+  pinMode(13, OUTPUT);
+
+  Serial.println("Ready !!");
 }
 
 void loop() {
@@ -30,7 +48,7 @@ void loop() {
 void processData(char *str) {
   
   String myString = String(str);
-  myString.replace(" ","");
+  myString.trim();
   int ind1 = myString.indexOf('-');  //finds location of first ,
   String item = myString.substring(0, ind1);
   String action = "";
@@ -45,9 +63,26 @@ void processData(char *str) {
   Serial.println(item);
   Serial.println(action);
   Serial.println(freq);
+
+  if (action =="ON") {
+    for (int theItem = 0; theItem < sizeof(outputName); theItem++) {
+      if (outputName[theItem] ==item) {
+        digitalWrite(outputPin[theItem], HIGH);
+        break;
+      }
+    }
+  } else if (action =="OF") {
+    for (int theItem = 0; theItem < sizeof(outputName); theItem++) {
+      if (outputName[theItem] ==item) {
+        digitalWrite(outputPin[theItem], LOW);
+        break;
+      }
+    }
+  }
 }
 
 //#L1-OF #L2-ON$500
+//#L6-ON
 
 
 
